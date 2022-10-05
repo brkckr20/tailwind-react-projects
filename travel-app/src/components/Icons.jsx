73bf607d25ae835edfe96f5050a842578d30bacb
Burak Çakır <burak2043@gmail.com>
2022-10-05 16:28:38 +0300

    travel-app started

---
+++ travel-app/src/components/Icons.jsx
@@ -0,0 +1,86 @@
+export const User = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        </svg>
+
+    )
+}
+
+export const Search = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+
+
+    )
+}
+
+
+export const Bars = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+        </svg>
+
+
+
+    )
+}
+
+
+export const Close = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+
+
+
+    )
+}
+
+export const Face = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
+        </svg>
+    )
+}
+
+export const Img = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+        </svg>
+
+    )
+}
+
+export const At = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
+        </svg>
+    )
+}
+
+
+export const Bolt = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+        </svg>
+
+    )
+}
+export const Cube = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+        </svg>
+
+
+    )
+}
---
+++ travel-app/src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { User, Search, Bars, Close, Face, Img, At, Bolt, Cube } from './Icons'
+
+const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+    const [logo,setLogo]=useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+        setLogo(!logo);
+    }
+
+    return (
+        <div className='flex justify-between items-center h-20 px-4'>
+            <div>
+                <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHEs.</h1>
+            </div>
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>View</li>
+                <li>Book</li>
+            </ul>
+            <div className='hidden md:flex'>
+                <Search />
+                <User />
+            </div>
+
+            {/* Hamburger */}
+            <div onClick={handleNav} className='md:hidden z-10'>
+                {
+                    nav ? <Close /> : <Bars />
+                }
+            </div>
+
+            {/* mobil menu dropdown  */}
+
+            <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+                <ul>
+                    <h1>BEACHEs.</h1>
+                    <li className='border-b '>Home</li>
+                    <li className='border-b '>Destinations</li>
+                    <li className='border-b '>Travel</li>
+                    <li className='border-b '>View</li>
+                    <li className='border-b '>Book</li>
+                    <div className='flex flex-col'>
+                        <button className='my-6'>Search</button>
+                        <button>Account</button>
+                    </div>
+                    <div className='icon flex justify-between my-6'>
+                        <Face />
+                        <Img />
+                        <At />
+                        <Bolt />
+                        <Cube />
+                    </div>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
