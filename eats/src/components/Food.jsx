f68012768260bd87481c7885ba836531b3ef69c9
Burak Çakır <burak2043@gmail.com>
2022-10-27 16:27:14 +0300

    food components completed

--- eats/src/components/Food.jsx
+++ eats/src/components/Food.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 import { data } from '../data/data'
 
 const Food = () => {
+
+    const [foods, setFoods] = useState(data);
+
     return (
         <div className='max-w-[1640px] mx-auto px-4 py-12'>
             <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
@@ -31,8 +34,22 @@ const Food = () => {
                 </div>
             </div>
             {/* display foods */}
-            <div>
-
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+                {
+                    foods.map((item, index) => (
+                        <div key={index} className="border shadow-lg hover:scale-105 duration-300 rounded-lg">
+                            <img src={item.image} alt={item.name} className="w-full h-[200px] object-cover rounded-t-lg" />
+                            <div className='flex justify-between px-2 py-4'>
+                                <p className='font-bold'>{item.name}</p>
+                                <p>
+                                    <span className='bg-orange-500 text-white p-1 rounded-full'>
+                                        {item.price}
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
